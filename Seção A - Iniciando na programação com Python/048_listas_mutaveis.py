feacfd2f4d7ff82d813719eeea164d521caaef98
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2024-04-29 00:27:02 -0300

    inciando listas ...

---
+++ Seção A - Iniciando na programação com Python/048_listas_mutaveis.py
@@ -0,0 +1,10 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+lista = []
+print(lista)
